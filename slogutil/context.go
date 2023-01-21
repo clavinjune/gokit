f7107d5ce8215e811ce80e4201039510ce5af7cc
clavinjune <24659468+clavinjune@users.noreply.github.com>
2023-01-22 06:27:39 +0700

    add slogutil context function
    Signed-off-by: clavinjune <24659468+clavinjune@users.noreply.github.com>

---
+++ slogutil/context.go
@@ -0,0 +1,24 @@
+package slogutil
+
+import (
+	"context"
+
+	"golang.org/x/exp/slog"
+)
+
+type key struct{}
+
+var keyInst *key = &key{}
+
+func From(ctx context.Context) *slog.Logger {
+	val, ok := ctx.Value(keyInst).(*slog.Logger)
+	if !ok {
+		return slog.Default()
+	}
+
+	return val
+}
+
+func Put(ctx context.Context, log *slog.Logger) context.Context {
+	return context.WithValue(ctx, keyInst, log)
+}
---
+++ slogutil/context_test.go
@@ -0,0 +1,49 @@
+package slogutil_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/clavinjune/gokit/slogutil"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/slog"
+)
+
+func TestMain(m *testing.M) {
+	slogutil.New(&slogutil.DefaultOption)
+	m.Run()
+}
+
+func TestFrom(t *testing.T) {
+	customized := slog.Default().With(
+		slog.Bool("default", false),
+	)
+
+	tt := []struct {
+		Name     string
+		Context  context.Context
+		Expected *slog.Logger
+	}{
+		{
+			Name:     "from empty context, should return default",
+			Context:  context.Background(),
+			Expected: slog.Default(),
+		},
+		{
+			Name:     "from filled context, should not return default",
+			Context:  slogutil.Put(context.Background(), customized),
+			Expected: customized,
+		},
+	}
+
+	for i := range tt {
+		tc := tt[i]
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			r := require.New(t)
+
+			actual := slogutil.From(tc.Context)
+			r.Equal(tc.Expected, actual)
+		})
+	}
+}
